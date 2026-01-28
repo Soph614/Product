2b1610b2ea815ea9c9ad625febefe9006f40503b
Soph614 <broylesa@mail.uc.edu>
2026-01-27 19:50:01 -0500

    Add files via upload

--- Product/Test/ProductTest.java
+++ Product/Test/ProductTest.java
@@ -1,18 +1,48 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
+    Product linen;
+
+    @BeforeEach
+    void setUp() {
+        linen = new Product("000001", "Linen", "Fine woven; natural color", 18.99);
+    }
+
+    @Test
+    void setIDTest() {
+        linen.setID("C000001");
+        assertEquals("C000001", linen.getID());
+    }
+
+    @Test
+    void setNameTest() {
+        linen.setName("Stained Linen");
+        assertEquals("Stained Linen", linen.getName());
+    }
+
+    @Test
+    void setDescriptionTest() {
+        linen.setDescription("Fine woven; natural color; stain spots");
+        assertEquals("Fine woven; natural color; stain spots", linen.getDescription());
+    }
+
+    @Test
+    void setCostTest() {
+        linen.setCost(9.99);
+        assertEquals(9.99, linen.getCost());
+    }
+
     @Test
     void toCSVTest() {
-        Product linen = new Product("000001", "Linen", "Fine woven; natural color", 18.99);
         assertEquals("000001, Linen, Fine woven; natural color, 18.99", linen.toCSV());
     }
 
     @Test
     void toJSONTest() {
-        Product linen = new Product("000001", "Linen", "Fine woven; natural color", 18.99);
         char DQ = '\u0022';
         assertEquals("{" + DQ + "ID" + DQ + ":" + DQ + "000001" + DQ + ", "
                 + DQ + "name" + DQ + ":" + DQ + "Linen" + DQ + ", "
@@ -22,7 +52,6 @@ class ProductTest {
 
     @Test
     void toXMLTest() {
-        Product linen = new Product("000001", "Linen", "Fine woven; natural color", 18.99);
         assertEquals("<Product>" +
                                 "<ID>000001</ID>" +
                                 "<name>Linen</name>" +
--- Product/src/Product.java
+++ Product/src/Product.java
@@ -4,38 +4,75 @@ public class Product {
     private String description;
     double cost;
 
+
+    /**  getCost
+     * Gets the cost of a product from a data record
+     * @return Returns the cost of the product
+     */
     public double getCost() {
         return cost;
     }
 
+    /**  setCost
+     * Changes a product's cost to an inputted value
+     * @param cost is what the cost is supposed to change to
+     */
     public void setCost(double cost) {
         this.cost = cost;
     }
 
+    /**  getID
+     * Gets a product's ID from a data record
+     * @return Returns the product's ID
+     */
     public String getID() {
         return ID;
     }
 
+    /**  setID
+     * Changes a product's ID to an inputted value
+     * @param ID is what the ID is supposed to change to
+     */
     public void setID(String ID) {
         this.ID = ID;
     }
 
+    /**  getDescription
+     * Gets a product's description from a data record
+     * @return Returns the product's description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**  setDescription
+     * Changes a product's description to an inputted String value
+     * @param description is what the product's description is supposed to change to
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**  getName
+     * Gets a product's name from a data record
+     * @return Returns the product's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Changes a product's name to an inputted String value
+     * @param name is what the product's name is supposed to change to
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**  toString
+     * Converts product data into a human-readable String
+     * @return Returns a human-readable string of product data
+     */
     @Override
     public String toString() {
         return "Product{" +
@@ -46,6 +83,14 @@ public class Product {
                 '}';
     }
 
+    /**  Product
+     * Creates a Product data record based on an inputted
+     * ID, name, description, and cost.
+     * @param ID is the product's ID number
+     * @param name is the product's name
+     * @param description is the product's description
+     * @param cost is the product's cost
+     */
     public Product(String ID, String name, String description, double cost) {
         this.ID = ID;
         this.name = name;
@@ -53,24 +98,20 @@ public class Product {
         this.cost = cost;
     }
 
-
-    /** toCSV()
+    /** toCSV
      * Takes data from a Product arraylist and
      * inputs it into a comma-separated value.
      * @return Returns a csv record with the product's
      * ID, name, description, and cost.
      */
-
     public String toCSV() {
         return getID() + ", " + getName() + ", " + getDescription() + ", " + getCost();
     }
 
-
-    /**  toJSON()
+    /**  toJSON
      * Converts an arraylist of product data into a JSON data string
      * @return Returns a JSON data String with product data
      */
-
     public String toJSON() {
         String returnString = "";
         char DQ = '\u0022';
@@ -82,11 +123,10 @@ public class Product {
         return returnString;
     }
 
-    /**  toXML()
+    /**  toXML
      * Converts an arraylist of product data into an XML data string
      * @return Returns an XML data String with product data
      */
-
     public String toXML() {
         String returnString = "";
         returnString = "<Product>";
